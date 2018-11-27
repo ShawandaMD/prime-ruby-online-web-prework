bc0492d3711dc1c4d78995207e6969bef43dc1da
Shawanda Davis <shawandadavis93@gmail.com>
2018-11-27 17:21:33 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,17 +1,2 @@
 require "pry"
 
-def prime?(x)
-  x = 1..
-  while x <= 1 || (x > 3 && x % x == 0 && ( x % 2 == 0 || x % 3 == 0 ))
-  return false
-  #else 
-  #return true
-  end
-  
-binding.pry
-end
-
-prime?(2)
-
-
-#x.each {|x| if
