70ad65e5d4158a1ecff519fbdd64419015dd6b67
Shawanda Davis <shawandadavis93@gmail.com>
2018-11-27 18:29:29 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -7,7 +7,7 @@ require "pry"
 
 def prime?(x) # define a method prime and it takes in a number as an argument
   x = (2..x-1) #create a range of numbers
-  array = (x).to_a
+  #array = (x).to_a
   if x <= 1  # create a conditional statement to determine if x is a prime number
     return False
   end
